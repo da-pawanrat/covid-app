641f75597bc3b68c015f0b8a2fde241ffd41efd8
Pawanrat <s6130613051@phuket.psu.ac.th>
2020-12-08 21:06:33 +0700

    create map into index page

--- database/database.js
+++ database/database.js
@@ -10,14 +10,34 @@ const pool = new Pool({
 })
 
 async function getdata(){
-    const sql = `select "Country/Region" as country , "Province/State" as province
-    from covid19_confirmed_csv`
+    const sql = `select "Country/Region" as country,"lat" as lat,"long" as long, "3/23/20" as confirm
+    from covid19_confirmed_csv 
+    where "3/23/20" > 0
+    order by "3/23/20" desc `
     const data = await pool.query(sql);
-    console.log(data);
+    //console.log(data);
     return data;
 
 }
+async function getTotal(){
+    const sql =`select covid19_confirmed_csv."Country/Region" as country, covid19_confirmed_csv."Province/State" as province, covid19_confirmed_csv."3/23/20" as date_conf,covid19_death_csv."3/23/20" as date_death,covid19_recovered_csv."3/23/20" as date_recover
+    from covid19_confirmed_csv , covid19_death_csv , covid19_recovered_csv
+    where covid19_confirmed_csv."Country/Region"= covid19_death_csv."Country/Region" and covid19_confirmed_csv."Province/State" = covid19_death_csv."Province/State" 
+    and covid19_confirmed_csv."Country/Region"= covid19_recovered_csv."Country/Region" and covid19_confirmed_csv."Province/State" = covid19_recovered_csv."Province/State"`
+    const data = await pool.query(sql);
+    return data;
+}
 
+async function getmap(){
+    const sql = `select "Country/Region" as country,lat as latitude ,long as longtitude , "3/23/20" as confirmed
+    from covid19_confirmed_csv `
+    const data = await pool.query(sql);
+    //console.log(data);
+    return data;
+
+}
 module.exports={
     getdata,
+    getTotal,
+    getmap,
 }
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,25 @@ var express = require('express');
 var router = express.Router();
 const db = require('../database/database')
 
-
 /* GET home page. */
 router.get('/',async function(req, res, next) {
+  const result = await db.getdata();
+  console.log(result.rows);
+  res.render('index', { resultData: result.rows});
+});
+router.get('/index',async function(req, res, next) {
   const result = await db.getdata();
   console.log(result.rows);
   res.render('index', { resultData: result.rows });
 });
 
-router.get('/table',async function(req, res, next) {
-  const result = await db.getdata();
+router.get('/country',async function(req, res, next) {
+  const result = await db.getTotal();
   console.log(result.rows);
-  res.render('table', { resultData: result.rows });
+  res.render('country', { resultData: result.rows });
 });
 
 
-
-
-
 module.exports = router;
+
+
